2f26c82c99a8e224846c760165902d30132af74f
Meena Rao <meenaraokallepu24@gmail.com>
2022-11-24 15:10:08 +0000

    gi

---
+++ ex.py
@@ -0,0 +1,4 @@
+# Write a Python program that asks for the baggage weight  and lets the user know if its weight exceeds the baggage limit of 15 kg. 
+x = int(input("enter your baggage weight"))
+if (x > 15) :
+    print("your baggage weight is exceed more than 15kgs")
---
+++ hello.py
@@ -0,0 +1,14 @@
+#print("meena")
+x=10
+def addition():   #functions
+    if (x < 15 ):
+        print("x is less than 15")
+        print(type(x))
+addition()
+#array
+a="meena"
+print(a[1])
+for x in a: #loop
+    print(x)
+
+    
